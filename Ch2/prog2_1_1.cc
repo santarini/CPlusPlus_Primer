2ea9cdfd81853ff83fef16841964d70b725dfdfb
Makoa <32736507+santarini@users.noreply.github.com>
2018-09-06 12:12:35 -1000

    Create prog2_1_1.cc

---
+++ Ch2/prog2_1_1.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int main(){
+
+	//basic arithmetic types
+	int a = 1;
+	short b = 2;
+	long c = 3;
+	long long d = 4;
+
+	//signed vs unsigned
+	signed int e = -5;
+	unsigned int f = -5;
+
+	//float v double
+	float g = 7.9012;
+	double h = 7.9012;
+
+	std::cout << a << std::endl;
+	std::cout << b << std::endl;
+	std::cout << c << std::endl;
+	std::cout << d << std::endl;
+	std::cout << e << std::endl;
+	std::cout << f << std::endl;
+	std::cout << g << std::endl;
+	std::cout << h << std::endl;
+
+}
